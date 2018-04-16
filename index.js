219240a9c8c325ce5fe8c5ec9a77bd1fd34c4c4e
sehmim <sehmim2010@gmail.com>
2018-04-16 16:54:44 -0400

    all you gotta do is make the render work

--- index.js
+++ index.js
@@ -15,13 +15,35 @@ const io = socket(server);
 
 io.set('origins', '*:*');
 
-io.on('connection', (socket) => {
+
+// io.on('connection', (socket) => {
+//     let handshakeData = socket.request;
+//     let username = handshakeData._query['userName'];
+//     io.emit('JOINED_ROOM', { username: username } );
+
+//     socket.on('SEND_MESSAGE', function(data) {
+//         console.log(data);
+//         io.emit('RECEIVE_MESSAGE', data);
+//     });
+// });
+
+io.sockets.on('connection', (socket) => {
     let handshakeData = socket.request;
     let username = handshakeData._query['userName'];
-    io.emit('JOINED_ROOM', { username: username } );
+    let current_room = 'RoomA';
+
+    socket.on('create', (room) => {
+        socket.leave(current_room);
+        socket.join(room);
+        current_room = room;
+        console.log('joined', room);
+        io.sockets.in(room).emit('JOINED_ROOM', { username: username, room: room } );
+        // io.sockets.in(room).emit('message', 'what is going on, party people?');
+    });
 
     socket.on('SEND_MESSAGE', function(data) {
         console.log(data);
-        io.emit('RECEIVE_MESSAGE', data);
+        io.sockets.in(current_room).emit('RECEIVE_MESSAGE', data);
+        // io.to(current_room).emit(data);
     });
-});
+})
